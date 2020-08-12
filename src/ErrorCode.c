4ad5ff0127113752a9ec6e1e9c705c45e5af0828
Ilan Prais <ilandovprais@gmail.com>
2020-08-12 19:51:40 +0300

    Added new error codes

--- src/ErrorCode.c
+++ src/ErrorCode.c
@@ -8,6 +8,14 @@ const char* error_getErrorMessage(const ErrorCode code) {
             return "The operation completed successfully";
         case ERROR_NOT_IMPLEMENTED:
             return "The procedure was not implemented";
+        case ERROR_MATRIX_MISSING:
+            return "The given matrix poiner points to null";
+        case ERROR_OUTPUT_MISSING:
+            return "The given output pointer points to null";
+        case ERROR_MATRIX_INCOMPATIBLE:
+            return "Cannot execute function on matrixes as the dimenions do not fit";
+        case  ERROR_FAILED_ALLOCATION:
+            return "Dymanic memory allocation failed";
         default:
             return "Unexpected error";
     }
